4f6ed9dfa0bd9f9a175f99bcee92fd484b25c828
Manmohan506 <manmohan@zelarsoft.com>
2021-05-21 23:32:01 +0530

    2

--- print.sh
+++ print.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 echo Hello World
+
+## Multi Line Output
+
+echo Hai Manmohan
+echo Bye Manmohan
+
+# Single echo command but multi line output
+# Syntax: echo -e "Line1/nLine2"
+
+echo -e "Hai1/nBye1"
+
