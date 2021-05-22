1572def8111a5f57c27141990e4208e215f31889
Manmohan506 <manmohan@zelarsoft.com>
2021-05-22 23:43:16 +0530

    function

---
+++ 06-functions.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+## Declare the function
+function sample1() {
+    echo Hello from Function1
+}
+
+sample2(){
+    echo Hello from Function2
+}
+
+## Access the function
+
+sample1
+sample2
