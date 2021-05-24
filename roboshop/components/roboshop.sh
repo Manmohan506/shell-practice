0a36330c8e101d8a68a9cc57e8f905ac40d4c773
Manmohan506 <manmohan@zelarsoft.com>
2021-05-24 23:51:14 +0530

    roboshop

---
+++ roboshop/components/roboshop.sh
@@ -0,0 +1,7 @@
+Fundamentally project components are designed to run on indivisual machine, But not all on one machine.
+For each component we can write a separate script or one script ensure it calls the appropriate component
+keep the code dry, because there are common components.
+Re run of a scipt should never fail
+
+## out of scope
+1.security
